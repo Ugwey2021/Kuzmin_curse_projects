2b128e34a00ac3ede30dd83389600199d1dc6c6c
ugwey2021@gmail.com <ugwey2021@gmail.com>
2023-11-21 22:43:11 +0500

    Initial commit

---
+++ app/src/main/java/com/example/curse_project/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.curse_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
